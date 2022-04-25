41b9de677cad8be27e1bc95a70bfdb19fb7917fb
Geovana44 <sgchaconc@unal.edu.co>
2022-04-25 17:25:22 -0500

    Clases
    Clases

--- SofiaChacón/Repasoclases.py
+++ SofiaChacón/Repasoclases.py
@@ -1,1 +1,29 @@
  #########Repaso clases 
+import numpy as np
+ 
+class ManejoMatriz():
+    """
+    Clase para calcular el determinante de una matriz de dim n 
+
+    """
+    def __init__(self,dim):
+        self.dim=dim
+        self.matriz=np.random.randint(0,10,(self.dim,self.dim)) 
+        self.rango = range(self.dim)
+    
+    def cal_DET(self):
+        determimante=  self.matriz[3][2]
+        return self.matriz, determimante
+        
+    def cal_TS(self):
+        y=self.rango
+        Ts=list(map(lambda y: self.matriz[y][y:self.dim], y))
+        return Ts
+    
+    
+
+x =ManejoMatriz(4)
+print(x.cal_DET())
+
+y =ManejoMatriz.cal_DET(4)
+print(y)
