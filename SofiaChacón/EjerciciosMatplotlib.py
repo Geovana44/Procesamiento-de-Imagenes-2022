583639a3a1ead6dd149400eeb706874ef5e585b0
Geovana44 <sgchaconc@unal.edu.co>
2022-06-07 12:30:40 -0500

    Tarea PDI
    Trea metricas y ROC

--- SofiaChacón/EjerciciosMatplotlib.py
+++ SofiaChacón/EjerciciosMatplotlib.py
@@ -37,3 +37,54 @@ ax.set_ylabel('sine')
 plt.show()
 
 ######EJERCICO 5#############
+y = [1, 4, 9, 16, 25,36,49, 64]
+x1 = [1, 16, 30, 42,55, 68, 77,88]
+x2 = [1,6,12,18,28, 40, 52, 65]
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+l1 = ax.plot(x1,y,'ys-') # solid line with yellow colour and square marker
+l2 = ax.plot(x2,y,'go--') # dash line with green colour and circle marker
+ax.legend(labels = ('tv', 'Smartphone'), loc = 'lower right') # legend placed at lower right
+ax.set_title("Advertisement effect on sales")
+ax.set_xlabel('medium')
+ax.set_ylabel('sales')
+plt.show()
+
+####EJERCICIO 6#############
+x = np.arange(0, math.pi*2, 0.05)
+fig=plt.figure()
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
+axes2 = fig.add_axes([0.55, 0.55, 0.3, 0.3]) # inset axes
+y = np.sin(x)
+axes1.plot(x, y, 'b')
+axes2.plot(x,np.cos(x),'r')
+axes1.set_title('sine')
+axes2.set_title("cosine")
+plt.show()
+
+######EJERCICIO 7############
+fig,a =  plt.subplots(2,2)
+x = np.arange(1,5)
+a[0][0].plot(x,x*x)
+a[0][0].set_title('square')
+a[0][1].plot(x,np.sqrt(x))
+a[0][1].set_title('square root')
+a[1][0].plot(x,np.exp(x))
+a[1][0].set_title('exp')
+a[1][1].plot(x,np.log10(x))
+a[1][1].set_title('log')
+plt.show()
+
+#####EJERCICO 8#############
+fig, axes = plt.subplots(1,3, figsize = (12,4))
+x = np.arange(1,11)
+axes[0].plot(x, x**3, 'g',lw=2)
+axes[0].grid(True)
+axes[0].set_title('default grid')
+axes[1].plot(x, np.exp(x), 'r')
+axes[1].grid(color='b', ls = '-.', lw = 0.25)
+axes[1].set_title('custom grid')
+axes[2].plot(x,x)
+axes[2].set_title('no grid')
+fig.tight_layout()
+plt.show()
